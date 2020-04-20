d4b4d45cee6ac1d7944574b4b40a385867d6fd6a
amakaUche <62053127+amakaUche@users.noreply.github.com>
2020-04-19 20:31:50 -0700

    first commit

---
+++ javascript3.js
@@ -0,0 +1,32 @@
+function myNumber(num) {
+    var arr = [];
+
+    for (var i = 1; i <= num; i++) {
+        if(i%2 == 0 || i%3 ==0 || i%5 ==0) {
+            if(i%2 == 0 && i%3 == 0 && i%5 ==0) {
+            arr.push("yu-gi-oh");
+        }
+        if(i%2 == 0 && i%3 == 0) {
+            arr.push("yu-gi");
+        }
+        if(i%2 == 0 && i%5 ==0) {
+            arr.push("yu-oh");
+        }
+        if(i%3 == 0 && i%5 == 0) {
+            arr.push("gi-oh");
+        }
+        if(i%2 == 0) {
+            arr.push("yu");
+        }
+        if(i%3 ==0) {
+            arr.push("gi");
+        }
+    if(i%5 ==0) {
+        arr.push("oh");
+        }
+    else {
+        arr.push(i);
+    }
+        }
+    } 
+
